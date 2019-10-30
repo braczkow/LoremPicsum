486bf098fb43e0f514208d5abda18db4253c3cc0
Bartosz Raczkowski <bartosz.raczkowski@meelogic.com>
2019-10-30 20:22:49 +0100

    Add and initialize Timber

--- app/src/main/java/com/braczkow/lorempicsum/app/App.kt
+++ app/src/main/java/com/braczkow/lorempicsum/app/App.kt
@@ -1,7 +1,15 @@
 package com.braczkow.lorempicsum.app
 
 import android.app.Application
+import com.braczkow.lorempicsum.BuildConfig
+import timber.log.Timber
 
 class App : Application(){
+    override fun onCreate() {
+        super.onCreate()
 
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
 }
