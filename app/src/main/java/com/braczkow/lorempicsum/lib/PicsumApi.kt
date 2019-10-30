e2221da28fd56ba79cd225d47addbafa259d9a6a
Bartosz Raczkowski <bartosz.raczkowski@meelogic.com>
2019-10-30 20:50:11 +0100

    Prototype getPicsList

--- app/src/main/java/com/braczkow/lorempicsum/lib/PicsumApi.kt
+++ app/src/main/java/com/braczkow/lorempicsum/lib/PicsumApi.kt
@@ -1,4 +1,14 @@
 package com.braczkow.lorempicsum.lib
 
+import io.reactivex.Single
+import retrofit2.http.GET
+
 interface PicsumApi {
+
+    data class ListResponse(val entries: List<Entry>) {
+        data class Entry(val id: String, val author: String, val width: String, val height: String, val url: String, val download_url: String)
+    }
+
+    @GET("v2/list")
+    fun getPicsList(): Single<ListResponse>
 }
